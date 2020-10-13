75e107502fd0dd73e8abd782e46b10464d373985
zhangh165 <dsa4444446@qq.com>
2020-10-13 21:29:00 +0800

    添加arrayToListNode方法

--- src/com/zhh1011/algorithmPractice/Util/ListNode.java
+++ src/com/zhh1011/algorithmPractice/Util/ListNode.java
@@ -1,7 +1,35 @@
 package com.zhh1011.algorithmPractice.Util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ListNode {
     public int val;
     public ListNode next;
     public ListNode(int x) { val = x; }
+    public ListNode(){}
+
+    /**
+     * 生成一个链表
+     * @param array 建议这样产生参数 "1,2,3,4,5".split(",")
+     * @return  返回链表头
+     */
+    public static ListNode arrayToListNode(String[] array){
+        if(null == array|| array.length == 0){
+            return null;
+        }
+        try {
+            List<Integer> list = Arrays.stream(array).map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            List<ListNode> result = list.stream().map(ListNode::new).collect(Collectors.toList());
+            for (int i = 0; i < result.size() - 1; i++) {
+                result.get(i).next = result.get(i + 1);
+            }
+            return result.get(0);
+        }catch (NumberFormatException e){
+            return null;
+        }
+    }
 }
