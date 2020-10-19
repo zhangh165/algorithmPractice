fc444342217f9d919e59ad25366d605dd35ef688
zhangh165 <dsa4444446@qq.com>
2020-10-19 09:47:45 +0800

    2020-10-19 LT844 比较退格字符串

---
+++ src/com/zhh1011/algorithmPractice/leetcode/LT844_backspaceCompare.java
@@ -0,0 +1,54 @@
+package com.zhh1011.algorithmPractice.leetcode;
+
+/**
+ * author: dsa44
+ * create time: 2020/10/19 9:08
+ * TODO: 2020/10/19 抄官方题解，自己再写一遍
+ */
+public class LT844_backspaceCompare {
+    public static boolean getResult(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else {
+                if (i >= 0 || j >= 0) {
+                    return false;
+                }
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getResult("ab##","c#d#"));
+    }
+}
