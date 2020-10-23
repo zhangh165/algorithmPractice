ef35814c2b63bfcad0c3aa56ed53124cf4c63f40
zhangh165 <dsa4444446@qq.com>
2020-10-23 21:53:18 +0800

    背包问题

--- src/com/zhh1011/algorithmPractice/unRecordPractice/Beibao.java
+++ src/com/zhh1011/algorithmPractice/unRecordPractice/Beibao.java
@@ -6,11 +6,23 @@ package com.zhh1011.algorithmPractice.unRecordPractice;
  */
 public class Beibao {
 
-    public static void getResult(int time) {
+    public static int getResult(int v,int[] value,int[] w) {
+        int[] dp = new int[v+1];
 
+        for(int i = 0;i < value.length;i++){
+            for(int j = v; j >=w[i];j--){
+                dp[j] = Math.max(dp[j],dp[j-w[i]]+value[i]);
+            }
+        }
+
+        int result = -1;
+        for(int i:dp){
+            result = Math.max(result,i);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(getResult(8,new int[]{4,5,2,1,3},new int[]{3,5,1,2,2}));
     }
 }
